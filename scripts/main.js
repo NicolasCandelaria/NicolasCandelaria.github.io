c91ba010e20219a1c9f6f7429231b82faaa62e7a
Nicolas Candelaria <nicolascandelaria8@gmail.com>
2021-01-18 09:10:18 -0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    
+
+    // smooth scroll
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+          event.preventDefault();
+
+          var hash = this.hash;
+
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+
+});
